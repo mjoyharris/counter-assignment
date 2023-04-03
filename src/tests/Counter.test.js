6a9ac30b26567c4758b8de18819c07cd6a36f9e9
Maya Harris <mayajharris@gmail.com>
2023-04-03 10:53:16 -0400

    fixes

--- src/tests/Counter.test.js
+++ src/tests/Counter.test.js
@@ -1,21 +1,25 @@
+// import the Counter component here
 import React from "react";
 import { render, fireEvent, screen } from "@testing-library/react";
 import Counter from "../components/counter";
 
-test("renders counter message", () => {
+beforeEach(() => {
+  // Render the Counter component here
   render(<Counter />);
+})
+
+test('renders counter message', () => {
   const messageElement = screen.getByText(/Counter/i);
   expect(messageElement).toBeInTheDocument();
 });
 
-test("should render initial count with value of 0", () => {
-  render(<Counter />);
+test('should render initial count with value of 0', () => {
   const countElement = screen.getByTestId("count");
   expect(countElement.textContent).toBe("0");
 });
 
-test("clicking + increments the count", () => {
-  render(<Counter />);
+test('clicking + increments the count', () => {
+  //render(<Counter />);
   const incrementButton = screen.getByText("+");
   const countElement = screen.getByTestId("count");
 
@@ -26,8 +30,7 @@ test("clicking + increments the count", () => {
   expect(countElement.textContent).toBe("2");
 });
 
-test("clicking - decrements the count", () => {
-  render(<Counter />);
+test('clicking - decrements the count', () => {
   const decrementButton = screen.getByText("-");
   const countElement = screen.getByTestId("count");
 
