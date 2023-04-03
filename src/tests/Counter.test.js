aa0c11f62ca949a2361eb6ea1e41b899f73ca587
Maya Harris <mayajharris@gmail.com>
2023-04-03 10:38:50 -0400

    Updated Counter.test.js

--- src/tests/Counter.test.js
+++ src/tests/Counter.test.js
@@ -1,22 +1,39 @@
-// import necessary react testing library helpers here
-// import the Counter component here
+import React from "react";
+import { render, fireEvent, screen } from "@testing-library/react";
+import Counter from "./counter";
 
-beforeEach(() => {
-  // Render the Counter component here
-})
-
-test('renders counter message', () => {
-  // Complete the unit test below based on the objective in the line above
+test("renders counter message", () => {
+  render(<Counter />);
+  const messageElement = screen.getByText(/Counter/i);
+  expect(messageElement).toBeInTheDocument();
 });
 
-test('should render initial count with value of 0', () => {
-  // Complete the unit test below based on the objective in the line above
+test("should render initial count with value of 0", () => {
+  render(<Counter />);
+  const countElement = screen.getByTestId("count");
+  expect(countElement.textContent).toBe("0");
 });
 
-test('clicking + increments the count', () => {
-  // Complete the unit test below based on the objective in the line above
+test("clicking + increments the count", () => {
+  render(<Counter />);
+  const incrementButton = screen.getByText("+");
+  const countElement = screen.getByTestId("count");
+
+  fireEvent.click(incrementButton);
+  expect(countElement.textContent).toBe("1");
+
+  fireEvent.click(incrementButton);
+  expect(countElement.textContent).toBe("2");
 });
 
-test('clicking - decrements the count', () => {
-  // Complete the unit test below based on the objective in the line above
+test("clicking - decrements the count", () => {
+  render(<Counter />);
+  const decrementButton = screen.getByText("-");
+  const countElement = screen.getByTestId("count");
+
+  fireEvent.click(decrementButton);
+  expect(countElement.textContent).toBe("-1");
+
+  fireEvent.click(decrementButton);
+  expect(countElement.textContent).toBe("-2");
 });
